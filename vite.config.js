fa32ff103bd97966590036fa783c26249a2c536a
shourya5204 <imailshourya@gmail.com>
2025-01-29 16:21:37 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/NASA-OrreryApp/",
+  plugins: [
+    react(),
+  ],
+  base: '/NASA-OrreryApp/',  // Replace with your repository name
 });
